25a1e2658f4567eca4f0134388323ee8ba3baf73
Nikita <baklykov.nikita369@gmail.com>
2021-09-20 19:37:00 +0300

    init5

--- src/com/company/practice3/practice3_3/Book.java
+++ src/com/company/practice3/practice3_3/Book.java
@@ -1,4 +1,34 @@
 package com.company.practice3.practice3_3;
 
 public class Book {
+    private String author;
+    private String name;
+    private int time;
+
+    public Book(String author, String name, int time) {
+        this.author = author;
+        this.name = name;
+        this.time = time;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", name='" + name + '\'' +
+                ", time=" + time +
+                '}';
+    }
 }
--- src/com/company/practice3/practice3_3/BookTest.java
+++ src/com/company/practice3/practice3_3/BookTest.java
@@ -1,5 +1,7 @@
 package com.company.practice3.practice3_3;
 
 public class BookTest {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Book red = new Book("Baklykov N.A", "mathematical circle", 2021);
+    }
 }
