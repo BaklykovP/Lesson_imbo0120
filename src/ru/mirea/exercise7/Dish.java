1124557b22895971c2cb3ff53c741da97fc640ea
BaklykovP <baklykov.nikita369@gmail.com>
2021-09-25 22:11:07 +0300

    exercise7

---
+++ src/ru/mirea/exercise7/Dish.java
@@ -0,0 +1,8 @@
+package ru.mirea.exercise7;
+
+public abstract class Dish {
+
+    public void calcVolume(){
+    }
+
+}
---
+++ src/ru/mirea/exercise7/Main.java
@@ -0,0 +1,26 @@
+package ru.mirea.exercise7;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        // Test Dish
+        Plate plate = new Plate(1,10);
+        Dish pan = new Pan(15,20);
+        Cup cup = new Cup("blue", 3, 10);
+        plate.makedFish();
+        ((Pan) pan).makeSoup();
+        cup.makedCoffee();
+        System.out.println("\n" + plate + "\n" + pan + "\n" + cup + "\n------------------");
+
+        // Test Dog
+        Dog labrador = new Labrador(5, 36, "white");
+        ((Labrador) labrador).actionLabrador();
+        System.out.println();
+        Corgi corgi = new Corgi(12, 14, "light brown");
+        corgi.actionCorgi();
+        System.out.println("\n" + labrador + "\n" + corgi + "\n");
+
+
+    }
+}
